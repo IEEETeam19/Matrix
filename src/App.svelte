5adcaf033e71027873e4999a93d9b5e40a4b0156
RiddheshVeling <velingriddhesh@gmail.com>
2023-01-22 23:10:39 +0530

    First Commit

---
+++ src/App.svelte
@@ -0,0 +1,117 @@
+<script>
+
+  import Box1 from "./lib/box1_R.svelte";
+  import Box3 from "./lib/box3_R.svelte";
+  import {array} from "../src/stores/data.js";
+  import {OutputArr, flag} from "../src/stores/outputData.js";
+  import Box2 from "./lib/box2_R.svelte";
+  import { each } from "svelte/internal";
+
+  let matrix = [];
+  let row = [];
+  let inputIndex = 0;
+  let rowIndex = 0;
+
+  function handleKeyPress(event) {
+    if (event.keyCode === 13) {
+      matrix[rowIndex] = row;
+      row = [];
+      rowIndex++;
+    }
+  }
+
+  function addToMatrix(event) {
+    let input = event.target;
+    row.push(parseInt(input.value));
+  }
+
+  let arr = []; 
+  export let a2 = [];
+  a2 = $OutputArr;
+  var a;
+  var l = []; 
+  
+  const num = (event)=>{
+    if(event.keyCode === 13)
+    {
+      a = event.target.value;
+      l = [...l, a];
+    }
+  }
+  
+  // '349', '349\n3', '349\n3\n8', '349\n3\n8\n4', '349\n3\n8\n4\n89'
+  const inputTake = function NL()
+  {
+    arr = [];
+    var i,j,ind;
+    i=j=0;
+    var str="z";
+
+    for(; i<l.length; i++)
+    {
+      var value;
+      if(i == 0)
+        value = parseInt(l[i], 10);
+
+      else if(i > 0)
+      {
+        ind = l[i].lastIndexOf('\n');
+        var strR = l[i].substring(ind+1);
+        value = parseInt(strR, 10);
+      }
+      arr = [...arr, value]; 
+    }
+  
+    array.set(arr);
+
+    // console.log(arr);
+    console.log("Actual Array: "+$array);
+
+  }
+
+  // document.getElementById("Op").innerHTML = "<p>Hello</p>";
+  
+</script>
+
+<main>
+
+<h2 class="font-bold text-center text-teal-900 text-lg">ONLINE NUMBER TOOLS</h2>
+<p class="text-center text-sm m-1"> <span class="bg-black text-teal-400 rounded px-1">Matrix Calculator</span> Choose from the following</p>
+
+<div class="bg-black w-full h-96 my-5">
+  <div class="text-white"> 
+    </div>
+    <div class="flex center-contents justify-evenly">
+        <div class="text-teal-700 mx-20 text-center my-4">
+            <p class="font-bold">INPUT</p>
+            <!--<input type="number" id="input" placeholder="Enter matrix element">-->
+            
+
+            <textarea class="text-white bg-teal-600 w-96 h-72" name="" id="input" cols="30" rows="10" on:keydown={handleKeyPress} on:input={addToMatrix}></textarea>
+            <!---->
+            <div>
+            <button on:click={inputTake} class="bg-teal-700 text-white rounded border-2 m-1 p-1 hover:bg-teal-900">SUBMIT</button>
+            </div>
+        </div>
+        <div class="text-teal-700 mx-20 text-center my-4">
+            <p class="font-bold">OUTPUT</p>
+            <div class="bg-teal-600 w-96 h-72 text-white" id="Op">
+              {#if $flag == 1}
+                {#each $OutputArr as ele}
+                  <ul>{ele}</ul>
+                {/each}
+              {/if}
+          </div>
+        </div>
+    </div>
+</div>
+
+  <div class="flex content-center mx-3 justify-evenly">
+    <Box1/>
+    <Box2/>
+    <Box3/>
+  </div>
+  <div class="text-center text-teal-800 font-bold">
+    END
+  </div>
+</main>
---
+++ src/lib/box1_R.svelte
@@ -0,0 +1,150 @@
+<script>
+
+    import {array} from "../stores/data.js"
+    import {OutputArr, flag} from "../stores/outputData.js"
+    
+    var nums, row, col, snum, lnum;
+    var fc = false;
+    export const outArr = [];
+    
+    const option1 = ()=>{
+
+    let outArr = [];
+    let rows = 6;
+    let columns = 6;
+    let min = snum;
+    let max = lnum;
+
+    function createMatrix() {
+        outArr = [];
+        for (let i = 0; i < rows; i++) {
+            outArr[i] = [];
+            for (let j = 0; j < columns; j++) {
+                outArr[i][j] = Math.floor(Math.random() * (max - min + 1) + min);
+            }
+        }
+    }
+    createMatrix();
+    OutputArr.set(outArr);
+    flag.set(1);
+
+    }
+
+    const option2 = ()=>{
+     
+    let outArr = [];
+    let rows = row;
+    let columns = col;
+    let min = snum;
+    let max = lnum;
+    outArr = new Array(rows);
+        for (let i = 0; i < rows; i++) {
+            outArr[i] = new Array(columns);
+            for (let j = 0; j < columns; j++) {
+                if(i === j) {
+                    outArr[i][j] = Math.floor(Math.random() * (max - min + 1) + min);
+                } else {
+                    outArr[i][j] = 0;
+                }
+            }
+        }
+
+        OutputArr.set(outArr);
+        flag.set(1);
+    }
+
+    const option3 = ()=> {
+        
+    let outArr = [];
+    let rows = row;
+    let min = snum;
+    let max = lnum;
+    outArr = new Array(rows);
+        for (let i = 0; i < rows; i++) {
+            outArr[i] = new Array(rows);
+            for (let j = 0; j < rows; j++) {
+                if (i <= j) {
+                    outArr[i][j] = Math.floor(Math.random() * (max - min + 1) + min);
+                } else {
+                    outArr[i][j] = outArr[j][i];
+                }
+            }
+        }
+        OutputArr.set(outArr);
+        flag.set(1);
+    }
+</script>
+
+<main>
+    <div class="bg-teal-700 text-white w-96 px-1" style="height: 420px;">
+        <p class="text-center font-bold">Generate Random Matrices</p>
+        <br>
+        <p class="text-justify text-xs">
+            You can generate random matrices of any size, shape, and form. 
+            All you need to do is enter the number of rows and columns in the options 
+            above as well as set the lowest and highest possible numeric values for random elements. 
+            You can also choose elements to be either integer or decimal numbers with the given precision. 
+</p>
+        
+        <div class="items-center mx-10">
+            <button class="bg-teal-900 w-full text-xs" on:click={option1}>
+                Random 6x6 matrix
+            </button>
+        </div>
+        
+        <div class="items-center mx-10">
+            <button class="bg-teal-900 w-full text-xs" on:click={option2}>
+                Random diagonal matrix
+            </button>
+        </div>
+
+        <div class="items-center mx-10">
+            <button class="bg-teal-900 w-full text-xs" on:click={option3}>
+                Random diagonal matrix
+            </button>
+        </div>
+
+        <hr class="m-2">
+
+        <h4 class="text-sm">Required options</h4>
+        <p class="text-xs">This is an online browser-based program for generating random matrices
+        </p>
+
+        <div class="content-center m-auto">
+            <input type="text" class="bg-teal-900 m-2 h-5 w-28 rounded text-sm" placeholder="number of rows" bind:value={row}>
+        </div>
+
+        <div class="content-center m-auto">
+            <input type="text" class="bg-teal-900 m-2 h-5 w-32 rounded text-sm" placeholder="number of columns" bind:value={col}>
+        </div>
+
+        <div class="content-center m-auto">
+            <input type="text" class="bg-teal-900 m-2 h-5 w-40 rounded text-sm" placeholder="smallest random number" bind:value={snum}>
+        </div>
+
+        <div class="content-center m-auto">
+            <input type="text" class="bg-teal-900 m-2 h-5 w-40 rounded text-sm" placeholder="largest random number" bind:value={lnum}>
+        </div>
+
+        <!--<div class="content-center m-auto">
+            <input type="text" class="bg-teal-900 m-2 h-5 w-52 rounded text-sm" placeholder="column delimiter of output matrix" bind:value={nums}>
+        </div>
+
+        <div class="content-center m-auto">
+            <input type="text" class="bg-teal-900 m-2 h-5 w-52 rounded text-sm" placeholder="row delimiter of output matrix" bind:value={nums}>
+        </div>-->
+        <!-- <input type="text" class="bg-teal-900 m-2 h-5 rounded text-sm" placeholder="\n">
+        <p class="text-xs">Input numbers are seperated by this symbol(new line)</p> -->
+        
+        <!-- <label for="op4" class="text-white text-xs font-bold">
+            <input type="checkbox" name="oprI" id="" bind:checked={fc}>
+                {#if fc}
+                    {sampleWithRepetitions}
+                {/if}
+                With Repetions
+        </label> -->
+
+        <!-- <p class="text-xs">Allowing choosing the same number for several time</p> -->
+
+    </div>
+</main>
